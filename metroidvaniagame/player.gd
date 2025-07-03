b16a8f7da8fce16f0899ccdf10773b5b487fa17e
LoganLaurenson <125315659+LoganLaurenson@users.noreply.github.com>
2025-07-03 20:28:33 +0100

    Add files via upload

--- metroidvaniagame/player.gd
+++ metroidvaniagame/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation = $AnimationPlayer
+@onready var jump_height_timer = $JumpHeightTimer
+@onready var coyote_timer = $CoyoteTimer
+const SPEED = 450.0
+const JUMP_VELOCITY = -550.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +13,8 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor || !coyote_timer.is_stopped():
+		jump_height_timer.start() 
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -23,3 +26,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	if Input.is_action_pressed("Left"):
+		animation.play("Attack_left")
+	elif Input.is_action_pressed("Right"):
+		animation.play("Attack_right")
+
+
+func _on_jump_height_timer_timeout() -> void:
+	if !Input.is_action_pressed("Jump"):
+		if velocity.y < -100:
+			velocity.y = -100
+			print("Low Jump")
+	else:
+		print("High Jump")
+	
+	var was_on_floor = is_on_floor()
+	
+	if was_on_floor && !is_on_floor():
+		coyote_timer.start()
